b3350e979815733ae86577ae9ef6bb96a4b57e6b
FDPcrewAJ <junga526@hsd.k12.or.us>
2026-02-24 13:21:38 -0800

    setup a04.cpp

---
+++ A04/a04.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+struct ArrayStack {
+    static const int MAX_SIZE = 100;
+    int value[MAX_SIZE];
+
+    void push(ArrayStack &stack, int value) {
+
+    }
+
+    int pop(ArrayStack &stack) {
+
+    }
+
+    int peek(ArrayStack &stack) {
+
+    }
+
+    bool isEmpty(ArrayStack stack) {
+
+    }
+
+    bool isFull(ArrayStack stack) {
+
+    }
+
+};
+
+struct ListStack {
+    int value;
+    ListStack* next;
+
+    void push(ListStack* &stack, int value) {
+
+    }
+
+    int pop(ListStack* &stack) {
+
+    }
+
+    int peek(const ListStack* stack) {
+
+    }
+
+    bool isEmpty(const  ListStack* stack) {
+
+    }
+    
+};
+
+int main() {
+
+    return 0;
+}
