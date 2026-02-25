9966f834aec68168389b6dc873389498ea5001ef
FDPcrewAJ <junga526@hsd.k12.or.us>
2026-02-24 21:04:36 -0800

    worked on a04

--- A04/a04.cpp
+++ A04/a04.cpp
@@ -3,27 +3,61 @@
 using namespace std;
 
 struct ArrayStack {
-    static const int MAX_SIZE = 100;
+    static const int MAX_SIZE = 10;
     int value[MAX_SIZE];
+    int count;
 
     void push(ArrayStack &stack, int value) {
-
+        // Check if stack is full
+        if (!isFull(stack)) {
+            // If not, shift elements right then add to front
+            for (int i = MAX_SIZE - 2; i >= 0; --i) {
+                stack.value[i + 1] = stack.value[i];
+            }
+            stack.value[0] = value;
+            stack.count++;
+        }
+        else {
+            cout << "Error: Stack is full!" << endl;
+        }
+        
     }
 
     int pop(ArrayStack &stack) {
+        // Check if array is empty
+        if (!isEmpty(stack)) {
+            // Get first value
+            int value = stack.value[0];
 
+            // Update the array
+            for (int i = 0; i < MAX_SIZE - 1; ++i) {
+                stack.value[i] = stack.value[i + 1];
+            }
+
+            stack.count--;
+            return value;
+
+        }
+        else {
+            cout << "Error: Stack is empty!" << endl;
+        }
     }
 
-    int peek(ArrayStack &stack) {
-
+    int peek(ArrayStack stack) {
+        if (!isEmpty(stack)) {
+            return stack.value[0];
+        }
+        else {
+            cout << "Error: Stack is empty!" << endl;
+        }
     }
 
-    bool isEmpty(ArrayStack stack) {
-
+    bool isEmpty(const ArrayStack &stack) {
+        return stack.count == -1;
     }
 
-    bool isFull(ArrayStack stack) {
-
+    bool isFull(const ArrayStack &stack) {
+        return stack.count == stack.MAX_SIZE - 1;
     }
 
 };
@@ -47,7 +81,7 @@ struct ListStack {
     bool isEmpty(const  ListStack* stack) {
 
     }
-    
+
 };
 
 int main() {
